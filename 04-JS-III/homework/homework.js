904683ef2eabbe977141da7204aaaa5919b7605e
Daamz <damianlacuadra8@gmail.com>
2022-08-28 21:03:00 -0300

    resolviendo homework

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -190,6 +190,7 @@ function mesesDelAño(array) {
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  
 }
 
 
